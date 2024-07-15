724a33c310dcc8bf7f023f2953d83867b6920688
zack-gtay <zackgtay@gmail.com>
2024-07-15 16:28:24 +0100

    change

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 try {
   await mongoose.connect(process.env.MONGO);
-  console.log("Connected to Database!");
+  console.log("Connected to db!");
 } catch (error) {
   console.log(error);
 }
